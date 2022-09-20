50b7a2f9e80277e53373109d53d97e0ae92f7dc8
Aidan Pine <hello@aidanpine.ca>
2022-09-20 21:25:05 +0000

    feat(tune): add autotune

---
+++ SmallTeamSpeech/tune.py
@@ -0,0 +1,25 @@
+from pytorch_lightning import Trainer
+
+from config import CONFIGS
+from dataloader import HiFiGANDataModule
+from model.vocoder.hifigan import HiFiGAN
+
+CONFIG = CONFIGS["base"]
+
+if CONFIG["training"]["strategy"] == "vocoder":
+    TRAINER = Trainer(
+        accelerator="auto",
+        devices="auto",
+        max_epochs=CONFIG["training"]["vocoder"]["max_epochs"],
+        auto_scale_batch_size=True,
+    )
+    VOCODER = HiFiGAN(CONFIG)
+    data = HiFiGANDataModule(CONFIG)
+    TRAINER.tune(VOCODER, data)
+
+if CONFIG["training"]["strategy"] == "feature_prediction":
+    pass
+
+if CONFIG["training"]["strategy"] == "e2e":
+    # see https://github.com/Lightning-AI/lightning/issues/7447#issuecomment-835695726
+    pass
