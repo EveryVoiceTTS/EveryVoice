aaf640650d29cc80eff2dacb1701654920a07a1d
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2023-09-21 15:12:22 -0400

    fix: the wizard should print rather than log messages
    Fixes #58

--- everyvoice/wizard/validators.py
+++ everyvoice/wizard/validators.py
@@ -1,32 +1,22 @@
 from pathlib import Path
 
-from loguru import logger
-
 
 def validate_path(value: str, is_dir=False, is_file=False, exists=False):
     path = Path(value).expanduser()
-    if is_dir and is_file or not is_dir and not is_file:
+    if bool(is_dir) == bool(is_file):
         raise ValueError(
             "The path must be either a file or directory, but both or neither were specified."
         )
-    if not is_file and path.is_file():
-        logger.warning(
-            f"Sorry, the path at '{path.absolute()}' is a file. Please select a directory."
-        )
+    if is_dir and path.is_file():
+        print(f"Sorry, the path '{path}' is a file. Please select a directory.")
         return False
-    if not is_dir and path.is_dir():
-        logger.warning(
-            f"Sorry, the path at '{path.absolute()}' is a file. Please select a directory."
-        )
+    if is_file and path.is_dir():
+        print(f"Sorry, the path '{path}' is a directory. Please select a file.")
         return False
     if not exists and path.exists():
-        logger.warning(
-            f"Sorry, the path at '{path.absolute()}' already exists. Please choose another path."
-        )
+        print(f"Sorry, the path '{path}' already exists. Please choose another path.")
         return False
     if exists and not path.exists():
-        logger.warning(
-            f"Sorry, the path at '{path.absolute()}' doesn't exist. Please choose another path."
-        )
+        print(f"Sorry, the path '{path}' doesn't exist. Please choose another path.")
         return False
     return True
