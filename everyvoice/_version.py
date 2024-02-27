2b66c49d67d1c92911d2352be05a0487ba3bc73f
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2024-02-27 15:24:23 -0500

    feat: ci to publish to pypi

--- everyvoice/_version.py
+++ everyvoice/_version.py
@@ -1,1 +1,5 @@
-VERSION = "0.1"
+# [Valid version numbers](https://packaging.python.org/en/latest/discussions/versioning/#versioning)
+# [PEP 440 – Version Identification and Dependency Specification](https://peps.python.org/pep-0440/)
+# [Specifying Your Project’s Version](https://setuptools.pypa.io/en/latest/userguide/distribution.html)
+# [N!]N(.N)*[{a|b|rc}N][.postN][.devN]
+VERSION = "0.1.0"
