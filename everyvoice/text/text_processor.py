17821d034ecffbe41ceb30f780b743931ffcffba
Aidan Pine <hello@aidanpine.ca>
2024-04-10 17:27:16 -0700

    refactor: set punctuation rule application everywhere

--- everyvoice/text/text_processor.py
+++ everyvoice/text/text_processor.py
@@ -358,7 +358,10 @@ class TextProcessor:
         if normalize_punctuation:
             tokens = self.apply_punctuation_rules(tokens)
         if encode_as_phonological_features:
-            return self.calculate_phonological_features(tokens).tolist()
+            # applying punctuation rules will have already happened here, so set to False
+            return self.calculate_phonological_features(
+                tokens, apply_punctuation_rules=False
+            ).tolist()
         else:
             # TODO: catch errors
             return [self._symbol_to_id[symbol] for symbol in tokens]
