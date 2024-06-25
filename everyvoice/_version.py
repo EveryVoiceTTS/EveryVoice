c4d7dbcd80ab3e86806eef936e4c8282921739b2
Aidan Pine <hello@aidanpine.ca>
2024-06-25 10:39:27 -0700

    chore: bump version

--- everyvoice/_version.py
+++ everyvoice/_version.py
@@ -2,4 +2,4 @@
 # [PEP 440 – Version Identification and Dependency Specification](https://peps.python.org/pep-0440/)
 # [Specifying Your Project’s Version](https://setuptools.pypa.io/en/latest/userguide/distribution.html)
 # [N!]N(.N)*[{a|b|rc}N][.postN][.devN]
-VERSION = "0.1.0a0.dev0"
+VERSION = "0.1.0a0"
