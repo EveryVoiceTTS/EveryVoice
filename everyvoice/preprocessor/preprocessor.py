0ac0b6c5c9192bebf722f6ac3d3223f0da673636
Aidan Pine <hello@aidanpine.ca>
2024-07-19 10:49:39 -0700

    test(preprocessor): add unittest for preprocessor

--- everyvoice/preprocessor/preprocessor.py
+++ everyvoice/preprocessor/preprocessor.py
@@ -894,19 +894,23 @@ class Preprocessor:
                     )
                 )
             )
+
+            if heavy_objective_evaluation:
+                # use objective metrics from https://pytorch.org/audio/main/tutorials/squim_tutorial.html
+                if sr != 16000:
+                    model_audio = torchaudio.functional.resample(audio, sr, 16000)
+                if len(model_audio.size()) == 1:  # must include channel
+                    model_audio = model_audio.unsqueeze(0)
+                stoi_hyp, pesq_hyp, si_sdr_hyp = model(model_audio)
+                data_point["stoi"] = float(stoi_hyp[0])
+                data_point["pesq"] = float(pesq_hyp[0])
+                data_point["si_sdr"] = float(si_sdr_hyp[0])
+
             assert (
                 len(audio.size()) == 1 or audio.size(0) == 1
             ), f"Audio has {audio.size(0)} channels, but should be mono"
             audio = audio.squeeze()
 
-            if heavy_objective_evaluation:
-                # use objective metrics from https://pytorch.org/audio/main/tutorials/squim_tutorial.html
-                if sr != 16000:
-                    audio = torchaudio.functional.resample(audio, sr, 16000)
-                stoi_hyp, pesq_hyp, si_sdr_hyp = model(audio)
-                data_point["stoi"] = float(stoi_hyp[0])
-                data_point["pesq"] = float(pesq_hyp[0])
-                data_point["si_sdr"] = float(si_sdr_hyp[0])
             if heavy_clip_detction:
                 _, total_clipping = detect_clipping(audio)
             else:
