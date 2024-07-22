7f96d078cc4ea4fd8a0d57f27bfceef47e2bbbe4
Aidan Pine <hello@aidanpine.ca>
2024-07-22 10:56:28 -0700

    feat: add model validator for text
    validator ensures that symbols are not defined as both punctuation and other symbols
    
    fixes #450

--- everyvoice/tests/test_wizard.py
+++ everyvoice/tests/test_wizard.py
@@ -1612,8 +1612,6 @@ class WavFileDirectoryRelativePathTest(TestCase):
                         SN.symbol_set_step.value: {
                             "characters": [
                                 " ",
-                                ",",
-                                ".",
                                 "A",
                                 "D",
                                 "E",
