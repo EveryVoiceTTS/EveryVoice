d2817b6491fc785326132076860be5f8fa872b53
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2024-11-12 17:48:46 -0500

    chore: schema update

--- everyvoice/_version.py
+++ everyvoice/_version.py
@@ -1,5 +1,5 @@
-# [Valid version numbers](https://packaging.python.org/en/latest/discussions/versioning/#versioning)
-# [PEP 440 – Version Identification and Dependency Specification](https://peps.python.org/pep-0440/)
-# [Specifying Your Project’s Version](https://setuptools.pypa.io/en/latest/userguide/distribution.html)
+# [Valid version numbers](https://packaging.python.org/en/latest/discussions/versioning/#versioning)  # noqa: E501
+# [PEP 440 – Version Identification and Dependency Specification](https://peps.python.org/pep-0440/)  # noqa: E501
+# [Specifying Your Project’s Version](https://setuptools.pypa.io/en/latest/userguide/distribution.html)  # noqa: E501
 # [N!]N(.N)*[{a|b|rc}N][.postN][.devN]
 VERSION = "0.2.0a0"
