51a3a7a34bee73f829717a441aff4697378e03fa
Aidan Pine <hello@aidanpine.ca>
2025-01-22 04:33:33 +0000

    fix: remove unused preprocessor kwarg

--- everyvoice/demo/app.py
+++ everyvoice/demo/app.py
@@ -72,7 +72,6 @@ def synthesize_audio(
         raise gr.Error("Speaker is not selected. Please select an output format.")
     config, device, predictions, callbacks = synthesize_helper(
         model=text_to_spec_model,
-        preprocessor=None,
         style_reference=style_reference,
         vocoder_model=vocoder_model,
         vocoder_config=vocoder_config,
