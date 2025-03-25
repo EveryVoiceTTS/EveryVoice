3e49bda27d6abe41a16978eada9ee85ec490dbbc
Eric Joanis <Eric.Joanis@nrc-cnrc.gc.ca>
2025-03-25 09:30:03 -0400

    fix: clean up the coverage reports from regression
    Generate the reports from the EveryVoice root so that reports are nicely relative.
    
    And fix the find command for combining parallel coverage data files.

--- everyvoice/tests/regression/combine-coverage.sh
+++ everyvoice/tests/regression/combine-coverage.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-find . -name .coverage\* | coverage combine --keep
-coverage report --include='*/everyvoice/*' | sed 's/.*EveryVoice\/everyvoice/everyvoice/' > coverage.txt
-coverage html --include='*/everyvoice/*'
-coverage xml --include='*/everyvoice/*'
-sed -i 's/"[^"]*EveryVoice.everyvoice/"everyvoice/g' coverage.xml
+find . -name .coverage.\* | xargs coverage combine --keep
+
+COVERAGE_DIR=$(pwd -P)
+EVERYVOICE_ROOT=$(python -c 'import everyvoice; print(everyvoice.__path__[0])')/..
+COVERAGE_OPTIONS=(--data-file="$COVERAGE_DIR/.coverage" --include='*/everyvoice/*')
+
+cd "$EVERYVOICE_ROOT" || exit 1
+coverage report "${COVERAGE_OPTIONS[@]}" > "$COVERAGE_DIR/coverage.txt"
+coverage html "${COVERAGE_OPTIONS[@]}" --directory="$COVERAGE_DIR/htmlcov"
+coverage xml "${COVERAGE_OPTIONS[@]}" -o "$COVERAGE_DIR/coverage.xml"
