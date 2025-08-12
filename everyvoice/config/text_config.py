95f12fa400bad4e5e10c5c265d6d3e53a8463a81
Jordan Lau <jordan.lau@nrc-cnrc.gc.ca>
2025-08-12 16:29:01 -0400

    feat: define boundaries in TextConfig
    This commit adds a field 'boundaries' to the TextConfig, in order to
    allow users to define custom strong and weak boundaries for specific
    languages. Note that this commit also refactors the chunk_text to
    require as input two lists of strong and weak boundaries.
    
    Fixes #659

--- everyvoice/config/text_config.py
+++ everyvoice/config/text_config.py
@@ -124,7 +124,7 @@ class Symbols(BaseModel):
         return self
 
 
-G2P_lang = Annotated[
+Language = Annotated[
     str,
     Field(
         title="Language ID",
@@ -139,8 +139,20 @@ G2P_py_module = Annotated[
     ),
 ]
 G2P_Engines = Annotated[
-    dict[G2P_lang, G2P_py_module],
+    dict[Language, G2P_py_module],
     Field(description="Mapping from language id to g2p module"),
+]
+LanguageBoundaries = Annotated[
+    dict[str, str],
+    Field(
+        title="Language Boundaries",
+        description="Strong and weak boundaries, for one language.",
+        examples=["{'strong': '!?.', 'weak': ':;,'}"],
+    ),
+]
+GlobalBoundaries = Annotated[
+    dict[Language, LanguageBoundaries],
+    Field(description="Dictionary of strong and weak boundaries, for every language."),
 ]
 
 
@@ -197,6 +209,12 @@ class TextConfig(ConfigModel):
         True,
         title="Split Text",
         description="Whether or not to perform text splitting (also referred to as text chunking) at inference time. Instead of synthesizing an entire utterance, the utterance will be split into smaller chunks and re-combined after synthesis. This can lead to more natural synthesis for long-form (i.e. paragraph) synthesis.",
+    )
+    boundaries: GlobalBoundaries = Field(
+        {},
+        title="Boundaries",
+        description="Strong and Weak boundaries on which text splitting is to be performed.",
+        examples=["""{'eng': {'strong': '!?.', 'weak': ':;,'}}'"""],
     )
 
     @model_validator(mode="after")
