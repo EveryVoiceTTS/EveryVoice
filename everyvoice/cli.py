32957cc29899e7496e234cb9ed41f118e434b55c
Eric Joanis <Eric.Joanis@nrc-cnrc.gc.ca>
2026-02-03 17:01:28 -0500

    feat: everyvoice check-text-config checks for missing symbols in config
    First step in implementing #385

--- everyvoice/cli.py
+++ everyvoice/cli.py
@@ -15,13 +15,14 @@ from rich import print as rich_print
 from rich.panel import Panel
 
 from everyvoice._version import VERSION
-from everyvoice.base_cli.checkpoint import inspect, rename_speaker
+from everyvoice.base_cli.checkpoint import inspect, load_checkpoint, rename_speaker
 from everyvoice.base_cli.interfaces import (
     inference_base_command_interface,
     typer_directory_option,
     typer_file_argument,
     typer_file_option,
 )
+from everyvoice.config.type_definitions import TargetTrainingTextRepresentationLevel
 from everyvoice.model.aligner.wav2vec2aligner.aligner.cli import (
     ALIGN_SINGLE_LONG_HELP,
     ALIGN_SINGLE_SHORT_HELP,
@@ -62,7 +63,7 @@ from everyvoice.model.vocoder.HiFiGAN_iSTFT_lightning.hfgl.cli import (
 )
 from everyvoice.model.vocoder.HiFiGAN_iSTFT_lightning.hfgl.cli import train as train_hfg
 from everyvoice.run_tests import SUITE_NAMES, run_tests
-from everyvoice.utils import spinner
+from everyvoice.utils import generic_psv_filelist_reader, spinner
 from everyvoice.wizard import (
     PREPROCESSING_CONFIG_FILENAME_PREFIX,
     SPEC_TO_WAV_CONFIG_FILENAME_PREFIX,
@@ -893,6 +894,147 @@ def g2p(
     g2p = get_g2p_engine(lang_id)
     for line in map(str.strip, input_file):
         print(g2p(line))
+
+
+@app.command()
+def check_text_config(  # noqa: C901
+    config: Annotated[
+        Optional[Path],
+        typer.Option(
+            help="path to text config, i.e., everyvoice-shared-text.yaml",
+            exists=True,
+            dir_okay=False,
+            file_okay=True,
+        ),
+    ] = None,
+    model: Annotated[
+        Optional[Path],
+        typer.Option(
+            help="path to a model whose text config will be used",
+            exists=True,
+            dir_okay=False,
+            file_okay=True,
+        ),
+    ] = None,
+    text_file: Annotated[
+        Optional[Path],
+        typer.Option(
+            help="path to a plain text file to check",
+            exists=True,
+            dir_okay=False,
+            file_okay=True,
+        ),
+    ] = None,
+    psv_file: Annotated[
+        Optional[Path],
+        typer.Option(
+            help="path to a psv file to check",
+            exists=True,
+            dir_okay=False,
+            file_okay=True,
+        ),
+    ] = None,
+    language: Annotated[
+        Optional[str],
+        typer.Option(
+            help="language id, required with --text-file, or for a csv file without a language column. "
+            + "Declaring the language is always required, because text normalization can be language specific, and g2p is always language specific."
+        ),
+    ] = None,
+):
+    """
+    Inspect a text configuration for compatiblity with an input file
+
+    Test processing input_file against the text configuration provided, or the text
+    configuration found in model, and report any incompatibilities.
+    """
+    if config and model:
+        raise typer.BadParameter("Please specify only one of --config or --model")
+    if not config and not model:
+        raise typer.BadParameter("One of --config or --model is required")
+    if text_file and psv_file:
+        raise typer.BadParameter("Please specify only one of --csv-file or --text-file")
+    if not text_file and not psv_file:
+        raise typer.BadParameter("One of --text-file or --csv-file is required")
+
+    if text_file:
+        with open(text_file, "r", encoding="utf8") as f:
+            text_lines = list(f)
+        # print(text_lines)
+        if language is None:
+            raise typer.BadParameter("--language is required with --text-file")
+        records = [{"characters": line, "language": language} for line in text_lines]
+    elif psv_file:
+        records = generic_psv_filelist_reader(psv_file)
+        if "language" not in records[0]:
+            if language is None:
+                raise typer.BadParameter(
+                    "--language is required for a csv file without a language column"
+                )
+            for record in records:
+                record["language"] = language
+
+    # Expensive imports are deferred so we fail fast where we can
+    with spinner("Loading software"):
+        from everyvoice.config.text_config import TextConfig
+        from everyvoice.preprocessor.preprocessor import Preprocessor
+        from everyvoice.text.text_processor import TextProcessor
+        from everyvoice.text.utils import guess_graphemes_in_text
+
+    if config:
+        text_config: TextConfig = TextConfig.load_config_from_path(config)
+    elif model:
+        with spinner("Loading model"):
+            checkpoint = load_checkpoint(model)
+        # print("Looking for text config")
+        model_config = checkpoint["hyper_parameters"]["config"]
+        if "text" in model_config:
+            # Question: FS2 models have text config, do any others have it?
+            # For other models that have it, are they in the same place in the metadata?
+            text_config = TextConfig(**model_config["text"])
+        else:
+            # Models without text config, e.g., a HiFiGan Vocoder, are not accepted here
+            raise typer.BadParameter(
+                f"Model/checkpoint {model} does not have an embedded text configuration"
+            )
+
+    # print(text_config)
+
+    text_processor_chars_only = TextProcessor(text_config)
+    text_processor_all = TextProcessor(text_config)
+    with spinner("Analyzing text"):
+        for record in records:
+            # print(record)
+            # Process just the text to calculate missing characters
+            _ = Preprocessor.process_text(
+                record,
+                text_processor_chars_only,
+                specific_text_representation=TargetTrainingTextRepresentationLevel.characters,
+            )
+            # Process all to also calculate missing phones
+            _ = Preprocessor.process_text(record, text_processor_all)
+
+    missing_characters = text_processor_chars_only.missing_symbols
+    missing_phones = text_processor_all.missing_symbols - missing_characters
+    missing_symbol_groups = list(missing_characters)
+    for missing_symbol_group in missing_symbol_groups:
+        split_symbols = guess_graphemes_in_text(missing_symbol_group)
+        if len(split_symbols) > 1:
+            count = missing_characters.pop(missing_symbol_group)
+            for symbol in split_symbols:
+                missing_characters[symbol] += count
+    # print("Missing characters", missing_characters)
+    # print("Missing phones", missing_phones)
+    if missing_characters:
+        print(
+            "The following characters are missing from your text config:",
+            sorted(missing_characters),
+        )
+    if missing_phones:
+        print(
+            "The following phones are missing from your text config:",
+            sorted(missing_phones),
+        )
 
 
 CLICK_APP = typer.main.get_group(app)
