6e45a2c8230e65489b0670402119b253a444a49a
Eric Joanis <Eric.Joanis@nrc-cnrc.gc.ca>
2026-02-24 11:35:50 -0500

    refactor: clean up the check_text_config code

--- everyvoice/cli.py
+++ everyvoice/cli.py
@@ -784,7 +784,7 @@ def demo(
                 print("\t config loaded")
             except Exception as e:
                 raise typer.BadParameter(
-                    f"Your config file {ui_config_file} has errors\n {e}"
+                    f"Your config file {ui_config_file} has errors.\n {e}"
                 )
     else:
         print("  - UI Config file path: None")
@@ -896,8 +896,69 @@ def g2p(
         print(g2p(line))
 
 
+def require_exactly_one_of(arg1: Any, arg1_name: str, arg2: Any, arg2_name: str):
+    if arg1 and arg2:
+        raise typer.BadParameter(
+            f"Please specify only one of {arg1_name} or {arg2_name}."
+        )
+    if not arg1 and not arg2:
+        raise typer.BadParameter(f"One of {arg1_name} and {arg2_name} is required.")
+
+
+def open_text_or_psv_file(
+    text_file: Optional[Path], psv_file: Optional[Path], language: Optional[str]
+) -> list[dict[str, str]]:
+    """helper for check_text_config: Open a text or psv file into records.
+
+    Language is required if not already in the psv
+
+    raises: typer.BadParameter if something is wrong"""
+    if text_file:
+        with open(text_file, "r", encoding="utf8") as f:
+            text_lines = list(f)
+        # print(text_lines)
+        if language is None:
+            raise typer.BadParameter("--language is required with --text-file.")
+        records = [{"characters": line, "language": language} for line in text_lines]
+    elif psv_file:
+        records = generic_psv_filelist_reader(psv_file)
+        if "language" not in records[0]:
+            if language is None:
+                raise typer.BadParameter(
+                    "--language is required for a psv file without a language column."
+                )
+            for record in records:
+                record["language"] = language
+    else:
+        assert False
+    return records
+
+
+def get_text_config_from_config_or_model(config: Optional[Path], model: Optional[Path]):
+    """Helper for chec_text_config: load a TextConfig from a config file or model file"""
+    from everyvoice.config.text_config import TextConfig
+
+    if config:
+        text_config: TextConfig = TextConfig.load_config_from_path(config)
+    elif model:
+        with spinner("Loading model"):
+            checkpoint = load_checkpoint(model)
+        # print("Looking for text config")
+        model_config = checkpoint["hyper_parameters"]["config"]
+        if "text" in model_config:
+            # Question: FS2 models have text config, do any others have it?
+            # For other models that have it, are they in the same place in the metadata?
+            text_config = TextConfig(**model_config["text"])
+        else:
+            # Models without text config, e.g., a HiFiGan Vocoder, are not accepted here
+            raise typer.BadParameter(
+                f"Model/checkpoint {model} does not have an embedded text configuration."
+            )
+    return text_config
+
+
 @app.command()
-def check_text_config(  # noqa: C901
+def check_text_config(
     config: Annotated[
         Optional[Path],
         typer.Option(
@@ -937,7 +998,7 @@ def check_text_config(  # noqa: C901
     language: Annotated[
         Optional[str],
         typer.Option(
-            help="language id, required with --text-file, or for a csv file without a language column. "
+            help="language id, required with --text-file, or for a psv file without a language column. "
             + "Declaring the language is always required, because text normalization can be language specific, and g2p is always language specific."
         ),
     ] = None,
@@ -948,56 +1009,18 @@ def check_text_config(  # noqa: C901
     Test processing input_file against the text configuration provided, or the text
     configuration found in model, and report any incompatibilities.
     """
-    if config and model:
-        raise typer.BadParameter("Please specify only one of --config or --model")
-    if not config and not model:
-        raise typer.BadParameter("One of --config or --model is required")
-    if text_file and psv_file:
-        raise typer.BadParameter("Please specify only one of --csv-file or --text-file")
-    if not text_file and not psv_file:
-        raise typer.BadParameter("One of --text-file or --csv-file is required")
-
-    if text_file:
-        with open(text_file, "r", encoding="utf8") as f:
-            text_lines = list(f)
-        # print(text_lines)
-        if language is None:
-            raise typer.BadParameter("--language is required with --text-file")
-        records = [{"characters": line, "language": language} for line in text_lines]
-    elif psv_file:
-        records = generic_psv_filelist_reader(psv_file)
-        if "language" not in records[0]:
-            if language is None:
-                raise typer.BadParameter(
-                    "--language is required for a csv file without a language column"
-                )
-            for record in records:
-                record["language"] = language
+    require_exactly_one_of(config, "--config", model, "--model")
+    require_exactly_one_of(text_file, "--text-file", psv_file, "--psv-file")
+    records = open_text_or_psv_file(text_file, psv_file, language)
 
     # Expensive imports are deferred so we fail fast where we can
     with spinner("Loading software"):
-        from everyvoice.config.text_config import TextConfig
+        from everyvoice.config.text_config import TextConfig  # noqa F401
         from everyvoice.preprocessor.preprocessor import Preprocessor
         from everyvoice.text.text_processor import TextProcessor
         from everyvoice.text.utils import guess_graphemes_in_text
 
-    if config:
-        text_config: TextConfig = TextConfig.load_config_from_path(config)
-    elif model:
-        with spinner("Loading model"):
-            checkpoint = load_checkpoint(model)
-        # print("Looking for text config")
-        model_config = checkpoint["hyper_parameters"]["config"]
-        if "text" in model_config:
-            # Question: FS2 models have text config, do any others have it?
-            # For other models that have it, are they in the same place in the metadata?
-            text_config = TextConfig(**model_config["text"])
-        else:
-            # Models without text config, e.g., a HiFiGan Vocoder, are not accepted here
-            raise typer.BadParameter(
-                f"Model/checkpoint {model} does not have an embedded text configuration"
-            )
-
+    text_config = get_text_config_from_config_or_model(config, model)
     # print(text_config)
 
     text_processor_chars_only = TextProcessor(text_config)
