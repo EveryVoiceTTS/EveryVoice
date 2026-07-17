f61ccc77d29209739e99c8db6ffcb449d1598c42
Eric Joanis <Eric.Joanis@nrc-cnrc.gc.ca>
2026-07-17 15:01:51 -0400

    refactor: move check-data and check-text-config into a group

--- everyvoice/cli.py
+++ everyvoice/cli.py
@@ -538,35 +538,7 @@ command(
     """,
 )(preprocess_styletts2)
 
-app.add_typer(
-    preprocess_group,
-    name="preprocess",
-    short_help="Preprocess your data",
-)
-
-
-# Add check_data to root
-command(
-    app,
-    name="check-data",
-    short_help="Check your data for outliers or any anomalies",
-    help="""
-    # Check Data Help
-
-    This command will check all of your data to help you find anomalies and outliers.
-
-    To check your data, make sure you've run preprocessing first (everyvoice preprocess --help).
-    Then you need to briefly and partially train a text-to-spec model. We recommend 100-1000 steps to start.
-
-    Then, with your partially trained model you can run the data checker:
-    \n\n
-    **everyvoice check-data config/everyvoice-text-to-spec.yaml logs_and_checkpoints/FeaturePredictionExperiment/base/checkpoints/last.ckpt**
-    \n\n
-
-    This will output two files - one containing some basic statistics for your data and the other containing losses for each datapoint as calculated by your model.
-
-    """,
-)(check_data_command)
+app.add_typer(preprocess_group, name="preprocess", short_help="Preprocess your data")
 
 
 # Add the train commands
@@ -678,17 +650,201 @@ app.add_typer(
     short_help="Download pretrained model weights from HuggingFace",
 )
 
-# Add the checkpoint commands
-checkpoint_group = typer.Typer(
-    **default_typer_args,
+# check group
+check_group = typer.Typer(**default_typer_args)
+
+# Add check_data to root
+command(
+    check_group,
+    name="data",
+    short_help="Check your data for outliers or any anomalies",
     help="""
-    # Checkpoint Help
+    # Check Data Help
 
-        - **inspect** --- Extract structural information from a checkpoint.
+    This command will check all of your data to help you find anomalies and outliers.
 
-        - **rename-speaker** --- Rename a speaker in the checkpoint's parameters.
+    To check your data, make sure you've run preprocessing first (everyvoice preprocess --help).
+    Then you need to briefly and partially train a text-to-spec model. We recommend 100-1000 steps to start.
+
+    Then, with your partially trained model you can run the data checker:
+    \n\n
+    **everyvoice check-data config/everyvoice-text-to-spec.yaml logs_and_checkpoints/FeaturePredictionExperiment/base/checkpoints/last.ckpt**
+    \n\n
+
+    This will output two files - one containing some basic statistics for your data and the other containing losses for each datapoint as calculated by your model.
+
     """,
-)
+)(check_data_command)
+
+
+def require_exactly_one_of(arg1: Any, arg1_name: str, arg2: Any, arg2_name: str):
+    if arg1 and arg2:
+        raise typer.BadParameter(
+            f"Please specify only one of {arg1_name} or {arg2_name}."
+        )
+    if not arg1 and not arg2:
+        raise typer.BadParameter(f"One of {arg1_name} and {arg2_name} is required.")
+
+
+def open_text_or_psv_file(
+    text_file: Optional[Path], psv_file: Optional[Path], language: Optional[str]
+) -> list[dict[str, str]]:
+    """helper for check_text_config: Open a text or psv file into records.
+
+    Language is required if not already in the psv
+
+    raises: typer.BadParameter if something is wrong"""
+    from everyvoice.utils import generic_psv_filelist_reader
+
+    if text_file:
+        with open(text_file, "r", encoding="utf8") as f:
+            text_lines = list(f)
+        # print(text_lines)
+        if language is None:
+            raise typer.BadParameter("--language is required with --text-file.")
+        records = [{"characters": line, "language": language} for line in text_lines]
+    else:
+        assert psv_file
+        records = generic_psv_filelist_reader(psv_file)
+        if "language" not in records[0]:
+            if language is None:
+                raise typer.BadParameter(
+                    "--language is required for a psv file without a language column."
+                )
+            for record in records:
+                record["language"] = language
+    return records
+
+
+def get_text_config_from_config_or_model(config: Optional[Path], model: Optional[Path]):
+    """Helper for chec_text_config: load a TextConfig from a config file or model file"""
+    from everyvoice.config.text_config import TextConfig
+    from everyvoice.utils import spinner
+
+    if config:
+        text_config: TextConfig = TextConfig.load_config_from_path(config)
+    else:
+        assert model
+        with spinner("Loading model"):
+            try:
+                checkpoint = load_checkpoint(model)
+            except Exception as e:
+                raise typer.BadParameter(
+                    f"Model/checkpoint '{model}' does not appear to be valid.\nError from loader: {e}"
+                )
+        # print("Looking for text config")
+        model_config = checkpoint["hyper_parameters"]["config"]
+        if "text" in model_config:
+            # Question: FS2 models have text config, do any others have it?
+            # For other models that have it, are they in the same place in the metadata?
+            text_config = TextConfig(**model_config["text"])
+        else:
+            # Models without text config, e.g., a HiFiGan Vocoder, are not accepted here
+            raise typer.BadParameter(
+                f"Model/checkpoint '{model}' does not have an embedded text configuration."
+            )
+    return text_config
+
+
+@command(check_group, name="text-config")
+def check_text_config(
+    config: Annotated[
+        Optional[Path],
+        typer_file_option(
+            "--config",
+            "-c",
+            help="path to text config, i.e., everyvoice-shared-text.yaml",
+        ),
+    ] = None,
+    model: Annotated[
+        Optional[Path],
+        typer_file_option(
+            "--model", "-m", help="path to a model whose text config will be used"
+        ),
+    ] = None,
+    text_file: Annotated[
+        Optional[Path],
+        typer_file_option(help="path to a plain text file to check"),
+    ] = None,
+    psv_file: Annotated[
+        Optional[Path],
+        typer_file_option(help="path to a psv file to check"),
+    ] = None,
+    language: Annotated[
+        Optional[str],
+        typer.Option(
+            "--language",
+            "-l",
+            help="language id, required with --text-file, or for a psv file without a language column. "
+            + "Declaring the language is always required, because text normalization can be language specific, and g2p is always language specific.",
+        ),
+    ] = None,
+):
+    """
+    Inspect a text configuration for compatiblity with an input file
+
+    Test processing input_file against the text configuration provided, or the text
+    configuration found in model, and report any incompatibilities.
+
+    Required options: one of --config and --model, as well as one of --text-file and --psv-file.
+    """
+    from everyvoice.utils import spinner
+
+    require_exactly_one_of(config, "--config", model, "--model")
+    require_exactly_one_of(text_file, "--text-file", psv_file, "--psv-file")
+    records = open_text_or_psv_file(text_file, psv_file, language)
+
+    # Expensive imports are deferred so we fail fast where we can
+    with spinner("Loading software"):
+        from everyvoice.config.text_config import TextConfig  # noqa F401
+        from everyvoice.preprocessor.preprocessor import Preprocessor
+        from everyvoice.text.text_processor import TextProcessor
+        from everyvoice.text.utils import guess_graphemes_in_text
+
+    text_config = get_text_config_from_config_or_model(config, model)
+    # print(text_config)
+
+    text_processor_chars_only = TextProcessor(text_config)
+    text_processor_all = TextProcessor(text_config)
+    with spinner("Analyzing text"):
+        for record in records:
+            # print(record)
+            # Process just the text to calculate missing characters
+            _ = Preprocessor.process_text(
+                record,
+                text_processor_chars_only,
+                specific_text_representation=TargetTrainingTextRepresentationLevel.characters,
+            )
+            # Process all to also calculate missing phones
+            _ = Preprocessor.process_text(record, text_processor_all)
+
+    missing_characters = text_processor_chars_only.missing_symbols
+    missing_phones = text_processor_all.missing_symbols - missing_characters
+    missing_symbol_groups = list(missing_characters)
+    for missing_symbol_group in missing_symbol_groups:
+        split_symbols = guess_graphemes_in_text(missing_symbol_group)
+        if len(split_symbols) > 1:
+            count = missing_characters.pop(missing_symbol_group)
+            for symbol in split_symbols:
+                missing_characters[symbol] += count
+    # print("Missing characters", missing_characters)
+    # print("Missing phones", missing_phones)
+    if missing_characters:
+        print(
+            "The following characters are missing from your text config:",
+            sorted(missing_characters),
+        )
+    if missing_phones:
+        print(
+            "The following phones are missing from your text config:",
+            sorted(missing_phones),
+        )
+
+
+app.add_typer(check_group, name="check", short_help="Check your data and/or config")
+
+# Add the checkpoint commands
+checkpoint_group = typer.Typer(**default_typer_args)
 command(
     checkpoint_group,
     name="inspect",
@@ -1280,170 +1436,6 @@ def g2p(
     g2p = get_g2p_engine(lang_id)
     for line in map(str.strip, input_file):
         print(g2p(line))
-
-
-def require_exactly_one_of(arg1: Any, arg1_name: str, arg2: Any, arg2_name: str):
-    if arg1 and arg2:
-        raise typer.BadParameter(
-            f"Please specify only one of {arg1_name} or {arg2_name}."
-        )
-    if not arg1 and not arg2:
-        raise typer.BadParameter(f"One of {arg1_name} and {arg2_name} is required.")
-
-
-def open_text_or_psv_file(
-    text_file: Optional[Path], psv_file: Optional[Path], language: Optional[str]
-) -> list[dict[str, str]]:
-    """helper for check_text_config: Open a text or psv file into records.
-
-    Language is required if not already in the psv
-
-    raises: typer.BadParameter if something is wrong"""
-    from everyvoice.utils import generic_psv_filelist_reader
-
-    if text_file:
-        with open(text_file, "r", encoding="utf8") as f:
-            text_lines = list(f)
-        # print(text_lines)
-        if language is None:
-            raise typer.BadParameter("--language is required with --text-file.")
-        records = [{"characters": line, "language": language} for line in text_lines]
-    else:
-        assert psv_file
-        records = generic_psv_filelist_reader(psv_file)
-        if "language" not in records[0]:
-            if language is None:
-                raise typer.BadParameter(
-                    "--language is required for a psv file without a language column."
-                )
-            for record in records:
-                record["language"] = language
-    return records
-
-
-def get_text_config_from_config_or_model(config: Optional[Path], model: Optional[Path]):
-    """Helper for chec_text_config: load a TextConfig from a config file or model file"""
-    from everyvoice.config.text_config import TextConfig
-    from everyvoice.utils import spinner
-
-    if config:
-        text_config: TextConfig = TextConfig.load_config_from_path(config)
-    else:
-        assert model
-        with spinner("Loading model"):
-            try:
-                checkpoint = load_checkpoint(model)
-            except Exception as e:
-                raise typer.BadParameter(
-                    f"Model/checkpoint '{model}' does not appear to be valid.\nError from loader: {e}"
-                )
-        # print("Looking for text config")
-        model_config = checkpoint["hyper_parameters"]["config"]
-        if "text" in model_config:
-            # Question: FS2 models have text config, do any others have it?
-            # For other models that have it, are they in the same place in the metadata?
-            text_config = TextConfig(**model_config["text"])
-        else:
-            # Models without text config, e.g., a HiFiGan Vocoder, are not accepted here
-            raise typer.BadParameter(
-                f"Model/checkpoint '{model}' does not have an embedded text configuration."
-            )
-    return text_config
-
-
-@command(app)
-def check_text_config(
-    config: Annotated[
-        Optional[Path],
-        typer_file_option(
-            "--config",
-            "-c",
-            help="path to text config, i.e., everyvoice-shared-text.yaml",
-        ),
-    ] = None,
-    model: Annotated[
-        Optional[Path],
-        typer_file_option(
-            "--model", "-m", help="path to a model whose text config will be used"
-        ),
-    ] = None,
-    text_file: Annotated[
-        Optional[Path],
-        typer_file_option(help="path to a plain text file to check"),
-    ] = None,
-    psv_file: Annotated[
-        Optional[Path],
-        typer_file_option(help="path to a psv file to check"),
-    ] = None,
-    language: Annotated[
-        Optional[str],
-        typer.Option(
-            "--language",
-            "-l",
-            help="language id, required with --text-file, or for a psv file without a language column. "
-            + "Declaring the language is always required, because text normalization can be language specific, and g2p is always language specific.",
-        ),
-    ] = None,
-):
-    """
-    Inspect a text configuration for compatiblity with an input file
-
-    Test processing input_file against the text configuration provided, or the text
-    configuration found in model, and report any incompatibilities.
-
-    Required options: one of --config and --model, as well as one of --text-file and --psv-file.
-    """
-    from everyvoice.utils import spinner
-
-    require_exactly_one_of(config, "--config", model, "--model")
-    require_exactly_one_of(text_file, "--text-file", psv_file, "--psv-file")
-    records = open_text_or_psv_file(text_file, psv_file, language)
-
-    # Expensive imports are deferred so we fail fast where we can
-    with spinner("Loading software"):
-        from everyvoice.config.text_config import TextConfig  # noqa F401
-        from everyvoice.preprocessor.preprocessor import Preprocessor
-        from everyvoice.text.text_processor import TextProcessor
-        from everyvoice.text.utils import guess_graphemes_in_text
-
-    text_config = get_text_config_from_config_or_model(config, model)
-    # print(text_config)
-
-    text_processor_chars_only = TextProcessor(text_config)
-    text_processor_all = TextProcessor(text_config)
-    with spinner("Analyzing text"):
-        for record in records:
-            # print(record)
-            # Process just the text to calculate missing characters
-            _ = Preprocessor.process_text(
-                record,
-                text_processor_chars_only,
-                specific_text_representation=TargetTrainingTextRepresentationLevel.characters,
-            )
-            # Process all to also calculate missing phones
-            _ = Preprocessor.process_text(record, text_processor_all)
-
-    missing_characters = text_processor_chars_only.missing_symbols
-    missing_phones = text_processor_all.missing_symbols - missing_characters
-    missing_symbol_groups = list(missing_characters)
-    for missing_symbol_group in missing_symbol_groups:
-        split_symbols = guess_graphemes_in_text(missing_symbol_group)
-        if len(split_symbols) > 1:
-            count = missing_characters.pop(missing_symbol_group)
-            for symbol in split_symbols:
-                missing_characters[symbol] += count
-    # print("Missing characters", missing_characters)
-    # print("Missing phones", missing_phones)
-    if missing_characters:
-        print(
-            "The following characters are missing from your text config:",
-            sorted(missing_characters),
-        )
-    if missing_phones:
-        print(
-            "The following phones are missing from your text config:",
-            sorted(missing_phones),
-        )
 
 
 if __name__ == "__main__":
