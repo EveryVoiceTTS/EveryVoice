ee9ca2c2161560e8beba3c46d1a3258ebed4d3df
Eric Joanis <eric.joanis@nrc-cnrc.gc.ca>
2026-09-01 10:19:52 -0400

    refactor: replace typing.Union by |

--- everyvoice/model/e2e/config.py
+++ everyvoice/model/e2e/config.py
@@ -5,5 +5,5 @@ from everyvoice.model.e2e.StyleTTS2_lightning.styletts2.ev_config import (  # no
     StyleTTS2TrainingConfig,
 )
 
-# If more models, change to Union[StyleTTS2Config, OtherModel]
+# If more models, change to StyleTTS2Config | OtherModel
 E2EConfig = StyleTTS2Config
